5c4013c6f79cfd9894040458ab05866b0084cc3f
Taj <mdtajhassan19@gmail.com>
2023-09-15 16:53:42 +0530

    Added Third file

---
+++ test_third.py
@@ -0,0 +1,26 @@
+
+
+# sentence = "Hello this is a Python programming Language"
+sentence = "my name is mohammad taj hassan and python is good programming language and programming is fun"
+rev_char = sentence[::-1]
+# words = sentence.split()
+dictionary = {'fName': 'Mohammad Taj', 'lName': 'Hassan', 'age': '25', 'Role': 'Automation Tester'}
+# words.reverse()  # it reverse original list
+
+string = 'Hello World'
+# 'Hello Universe'
+
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+b = ['a', 'b', 'c', 'd']
+
+# '1a2b3c4d56789'
+
+l1 = [1, 0, 12, 5, 0, 45, 0]
+#   [1, 5, 12, 45, 0, 0, 0]
+
+
+
+
+
+
